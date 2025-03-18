c40086df2c71e71b076dbbd689617641dea02318
BorodiBogdan <bogdanborodiv@gmail.com>
2025-03-18 16:51:02 +0200

    Added cart page with delete functionality, added mock database and updated database queries

---
+++ SteamStore/Repositories/CartRepository.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Collections.ObjectModel;
+
+
+public class CartRepository
+{
+    private readonly DataLink dataLink;
+    private readonly User user;
+
+    public CartRepository(DataLink dataLink, User user)
+    {
+        this.dataLink = dataLink;
+        this.user = user;
+    }
+    public List<Game> getCartGames()
+    {
+        SqlParameter[] parameters = new SqlParameter[]
+        {
+            new SqlParameter("@user_id", user.UserId)
+        };
+
+        DataTable? result = dataLink.ExecuteReader("GetAllCartGames", parameters);
+        List<Game> games = new List<Game>();
+
+        if (result != null)
+        {
+            foreach (DataRow row in result.Rows)
+            {
+                Game game = new Game
+                {
+                    Id = (int)row["game_id"],
+                    Name = (string)row["name"],
+                    Description = (string)row["Description"],
+                    ImagePath = (string)row["image_url"],
+                    Price = Convert.ToDouble(row["price"]),
+                    Status = "Available"
+                };
+                games.Add(game);
+            }
+        }
+        return games;
+    }   
+    void addProductToCart(Game game)
+    {
+        SqlParameter[] parameters = new SqlParameter[]
+        {
+            new SqlParameter("@user_id", user.UserId),
+            new SqlParameter("@game_id", game.Id)
+        };
+
+        try
+        {
+            dataLink.ExecuteNonQuery("AddProductToCart", parameters);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+
+    public void removeGameFromCart(Game game)
+    {
+        SqlParameter[] parameters = new SqlParameter[]
+        {
+            new SqlParameter("@user_id", user.UserId),
+            new SqlParameter("@game_id", game.Id)
+        };
+
+        try
+        {
+            dataLink.ExecuteNonQuery("RemoveGameFromCart", parameters);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }   
+    }
+}
---
+++ SteamStore/ViewModels/CartViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+public class CartViewModel
+{
+    public ObservableCollection<Game> CartGames { get; set; }
+
+    public decimal TotalPrice => (decimal)CartGames.Sum(game => (double)game.Price);
+
+    private readonly CartService _cartService;
+
+    public CartViewModel(CartService cartService)
+    {
+        _cartService = cartService;
+        CartGames = new ObservableCollection<Game>();
+        LoadGames();
+    }
+
+    private void LoadGames()
+    {
+        var games = _cartService.getCartGames();
+        foreach (var game in games)
+        {
+            CartGames.Add(game);
+        }
+    }
+    public void RemoveGameFromCart(Game game)
+    {
+        _cartService.RemoveGameFromCart(game);
+        CartGames.Remove(game);
+    }
+}
