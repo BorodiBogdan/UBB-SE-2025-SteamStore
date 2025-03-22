65561af0b2280560a3a94c33bc0ad7d8748365bb
Chis-Denis <denis20chis@gmail.com>
2025-03-22 20:00:44 +0200

    Creca ca mere :(

--- SteamStore/Data/Schema/mockDatabaseQuery.sql
+++ SteamStore/Data/Schema/mockDatabaseQuery.sql
@@ -1022,9 +1022,8 @@ GO
 
 --SELECT * FROM games;
 --EXEC getGameTags @gid = "133";
---EXEC getGameTags @gid = "1";
---select * from games where game_id = 1;
+--EXEC getGameTags @gid = "4142";
 
---EXEC ValidateGame @game_id = "";
+--EXEC ValidateGame @game_id = "133";
 
 --EXEC getAllTags;
