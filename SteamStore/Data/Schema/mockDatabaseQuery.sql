6abcf221b74ae8f9ca253f959ea1238d76b704f9
BorodiBogdan <bogdanborodiv@gmail.com>
2025-03-24 20:20:29 +0200

    Added procedure to mockdatabase

--- SteamStore/Data/Schema/mockDatabaseQuery.sql
+++ SteamStore/Data/Schema/mockDatabaseQuery.sql
@@ -568,7 +568,21 @@ BEGIN
 END;
 GO
 
+DROP PROCEDURE IF EXISTS isGamePurchased
+GO
 
+CREATE PROCEDURE isGamePurchased
+@game_id INT,
+@user_id INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	
+	IF EXISTS (SELECT 1 FROM games_users WHERE game_id = @game_id AND user_id = @user_id AND is_purchased = 1)
+		SELECT 1 AS Result;
+	ELSE
+		SELECT 0 AS Result;
+END;
 
 DROP PROCEDURE IF EXISTS DeleteGameTags;
 GO
