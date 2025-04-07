24520c32d5f1146473377b3a4f08e3440d500f62
Eduard-David Jitareanu <eduard.jitareanu@stud.ubbcluj.ro>
2025-04-07 13:18:45 +0300

    tested GameService

--- SteamStore/Services/GameService.cs
+++ SteamStore/Services/GameService.cs
@@ -1,14 +1,16 @@
-﻿using SteamStore.Models;
-using SteamStore.Repositories.Interfaces;
-using SteamStore.Services.Interfaces;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using SteamStore.Models;
+using SteamStore.Repositories.Interfaces;
+using SteamStore.Services.Interfaces;
+
+namespace SteamStore.Services;
 
 public class GameService : IGameService
 {
-    private IGameRepository _gameRepository;
+    private readonly IGameRepository _gameRepository;
     public GameService(IGameRepository gameRepository)
     {
         _gameRepository = gameRepository;
@@ -26,33 +28,35 @@ public class GameService : IGameService
     public Collection<Tag> getAllGameTags(Game game)
     {
         return new Collection<Tag>(_gameRepository
-                       .getAllTags()
-                                  .Where(tag => game.Tags.Contains(tag.tag_name))
-                                             .ToList());
+            .getAllTags()
+            .Where(tag => game.Tags.Contains(tag.tag_name))
+            .ToList());
     }
 
-    public Collection<Game> searchGames(String search_query)
+    public Collection<Game> searchGames(string searchQuery)
     {
         return new Collection<Game>(_gameRepository
-       .getAllGames()
-       .Where(game => game.Name.ToLower().Contains(search_query.ToLower()))
-       .ToList());
+            .getAllGames()
+            .Where(game => game.Name.ToLower().Contains(searchQuery.ToLower()))
+            .ToList());
     }
 
-    public Collection<Game> filterGames(int minRating,int minPrice,int maxPrice, String[] Tags) {
+    public Collection<Game> filterGames(int minRating,int minPrice,int maxPrice, string[] tags)
+    {
+        if (tags == null) throw new ArgumentNullException(nameof(tags));
         return new Collection<Game>(
             _gameRepository.getAllGames()
-            .Where(game => game.Rating >= minRating &&
-                game.Price >= minPrice &&
-                game.Price <= maxPrice && 
-                (Tags.Length == 0 || Tags.ToList().All(tag => game.Tags.Contains(tag)))
-            )
-            .ToList());
+                .Where(game => game.Rating >= minRating &&
+                               game.Price >= minPrice &&
+                               game.Price <= maxPrice && 
+                               (tags.Length == 0 || tags.ToList().All(tag => game.Tags.Contains(tag)))
+                )
+                .ToList());
     }
 
     public void computeTrendingScores(Collection<Game> games)
     {
-        int maxRecentSales = games.Max(game => game.noOfRecentPurchases);
+        var maxRecentSales = games.Max(game => game.noOfRecentPurchases);
         foreach (var game in games)
         {
             game.trendingScore = (((float)game.noOfRecentPurchases) / maxRecentSales);
@@ -61,32 +65,33 @@ public class GameService : IGameService
 
     public Collection<Game> getTrendingGames()
     {
-        var games = _gameRepository.getAllGames();
+        return GetSortedAndFilteredVideoGames(_gameRepository.getAllGames());
+    }
+
+    private Collection<Game> GetSortedAndFilteredVideoGames(Collection<Game> games)
+    {
         computeTrendingScores(games);
         return new Collection<Game>(games
-                       .OrderByDescending(game => game.trendingScore)
-                       .Take(10)
-                       .ToList());
+            .OrderByDescending(game => game.trendingScore)
+            .Take(10)
+            .ToList());
     }
 
     public Collection<Game> getDiscountedGames()
     {
-        var games = _gameRepository.getAllGames();
-        computeTrendingScores(games);
-        return new Collection<Game>(games
-                        .Where(game => game.Discount>0)
-                        .OrderByDescending(game => game.trendingScore)
-                        .Take(10)
-                        .ToList());
+        var discountedGames = _gameRepository.getAllGames()
+            .Where(game => game.Discount > 0).ToList();
+        return GetSortedAndFilteredVideoGames(new Collection<Game>(discountedGames));
     }
 
     public List<Game> GetSimilarGames(int gameId)
     {
-        var allGames = getAllGames();
+        var randy = new Random(DateTime.Now.Millisecond);
+        var allGames = _gameRepository.getAllGames();
         return allGames
             .Where(g => g.Id != gameId)
-            .OrderBy(g => Guid.NewGuid()) // Random order
-            .Take(3) // Only take 3 similar games
+            .OrderBy(_ => randy.Next()) 
+            .Take(3)
             .ToList();
 
     }
